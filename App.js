50080deeff11d26b90cab6c35d473cc2ec70a69d
PrestonWinstead <prestonwinstead@me.com>
2018-01-12 10:44:09 -0600

    [setup] add eslint

--- App.js
+++ App.js
@@ -1,21 +1,33 @@
 import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
+import { StackNavigator } from 'react-navigation';
+import Login from './Login';
+import Dashboard from './Dashboard';
 
-export default class App extends React.Component {
-  render() {
-    return (
-      <View style={styles.container}>
-        <Text>Wander</Text>
-      </View>
-    );
-  }
-}
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+const App = StackNavigator({
+  Dashboard: {
+    screen: Dashboard,
+  },
+  Login: {
+    screen: Login,
   },
 });
+
+// export default class App extends React.Component {
+//   render() {
+//     return (
+//       <View style={styles.container}>
+//         <Text>Wander</Text>
+//       </View>
+//     );
+//   }
+// }
+
+// const styles = StyleSheet.create({
+//   container: {
+//     flex: 1,
+//     backgroundColor: '#fff',
+//     alignItems: 'center',
+//     justifyContent: 'center',
+//   },
+// });
