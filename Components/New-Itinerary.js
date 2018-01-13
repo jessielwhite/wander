8df682d951e3d80802b6fc924cf1f3c707d77ce5
PrestonWinstead <prestonwinstead@me.com>
2018-01-13 16:59:56 -0600

    [newItinerary] scaffold out create itinerary page

--- Components/New-Itinerary.js
+++ Components/New-Itinerary.js
@@ -1,6 +1,16 @@
 import React from 'react';
-import { Text, Button, View, StyleSheet, Image } from 'react-native';
+import {
+  Text,
+  TextInput,
+  Button,
+  View,
+  StyleSheet,
+  Image,
+  DatePickerIOS,
+  TouchableOpacity,
+} from 'react-native';
 import PropTypes from 'prop-types';
+import DateTimePicker from 'react-native-modal-datetime-picker';
 import logo from '../img/logo.png';
 
 const styles = StyleSheet.create({
@@ -8,16 +18,67 @@ const styles = StyleSheet.create({
     flex: 1,
     backgroundColor: '#fff',
     alignItems: 'center',
-    justifyContent: 'center',
   },
 });
 
 export default class NewItinerary extends React.Component {
+  constructor() {
+    super();
+    this.state = {
+      chosenDate: new Date(),
+      visibility: false,
+    };
+    this.setDate = this.setDate.bind(this);
+    this.showDateTimePicker = this.showDateTimePicker.bind(this);
+    this.hideDateTimePicker = this.hideDateTimePicker.bind(this);
+  }
+  setDate(chosenDate) {
+    this.setState({ chosenDate });
+    console.log(chosenDate);
+    this.hideDateTimePicker();
+  }
+
+  showDateTimePicker() {
+    this.setState({ visibility: true });
+  }
+
+  hideDateTimePicker() {
+    this.setState({ visibility: false });
+  }
+
   render() {
     return (
       <View style={styles.container}>
         <Image source={logo} style={{ width: 100, height: 100 }} />
-        <Text>New Itinerary</Text>
+        <TextInput
+          style={{
+            height: 100,
+            width: 300,
+            borderColor: 'gray',
+            borderWidth: 1,
+            borderRadius: 6,
+            marginBottom: 5,
+          }}
+          placeholder="Where are you wandering?"
+        />
+        <Text>When are you leaving?</Text>
+        <TouchableOpacity onPress={this.showDateTimePicker}>
+          <Text>Select a date</Text>
+        </TouchableOpacity>
+        <DateTimePicker
+          isVisible={this.state.visibility}
+          onConfirm={this.setDate}
+          onCancel={this.hideDateTimePicker}
+        />
+        <Text>When do you come back?</Text>
+        <TouchableOpacity onPress={this.showDateTimePicker}>
+          <Text>Select a date</Text>
+        </TouchableOpacity>
+        <DateTimePicker
+          isVisible={this.state.visibility}
+          onConfirm={this.setDate}
+          onCancel={this.hideDateTimePicker}
+        />
         <Button
           title="Go to Dashboard"
           onPress={() => this.props.navigation.navigate('Dashboard')}
