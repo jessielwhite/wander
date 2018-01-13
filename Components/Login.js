ddac84beb5aaa2b3ee61cc77b037c72bd5ee1848
PrestonWinstead <prestonwinstead@me.com>
2018-01-13 15:43:47 -0600

    [login] make input boxes move out of the way of keyboard

--- Components/Login.js
+++ Components/Login.js
@@ -1,5 +1,6 @@
 import React from 'react';
-import { TextInput, StyleSheet, View, Button, Image, Text } from 'react-native';
+import { TextInput, StyleSheet, View, Button, Image, Text, KeyboardAvoidingView } from 'react-native';
+import { KeyboardAwareScrollView } from 'react-native-keyboard-aware-scroll-view';
 import PropTypes from 'prop-types';
 import logo from '../img/logo.png';
 
@@ -10,45 +11,55 @@ const styles = StyleSheet.create({
     alignItems: 'center',
     justifyContent: 'center',
   },
+  textInput: {
+    height: 40,
+    width: 300,
+    borderColor: 'gray',
+    borderWidth: 1,
+    borderRadius: 6,
+    marginBottom: 5,
+  },
 });
 
 export default class Login extends React.Component {
+  constructor() {
+    super();
+    this.state = {
+      email: '',
+      password: '',
+    };
+    this.login = this.login.bind(this);
+  }
+  login() {
+    console.log(this.state.email);
+    console.log(this.state.password);
+    this.props.navigation.navigate('Dashboard');
+  }
   render() {
     return (
-      <View style={styles.container}>
+      <KeyboardAwareScrollView contentContainerStyle={styles.container}>
         <Image source={logo} style={{ width: 200, height: 200, marginBottom: 50 }} />
         <Text>Placeholder for Facebook</Text>
         <Text>Placeholder for Google</Text>
         <TextInput
-          style={{
-            height: 40,
-            width: 300,
-            borderColor: 'gray',
-            borderWidth: 1,
-            borderRadius: 6,
-            marginBottom: 5,
-          }}
+          style={styles.textInput}
           placeholder="email@email.com"
+          onChangeText={text => this.setState({ email: text })}
         />
         <TextInput
-          style={{
-            height: 40,
-            width: 300,
-            borderColor: 'gray',
-            borderWidth: 1,
-            borderRadius: 6,
-          }}
+          style={styles.textInput}
           placeholder="password"
+          onChangeText={text => this.setState({ password: text })}
         />
         <Button
           title="Log in"
-          onPress={() => this.props.navigation.navigate('Dashboard')}
+          onPress={this.login}
         />
         <Button
           title="Sign up"
           onPress={() => this.props.navigation.navigate('Signup')}
         />
-      </View>
+      </KeyboardAwareScrollView>
     );
   }
 }
