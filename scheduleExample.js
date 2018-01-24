703bcf49f4b52891204694b793f4f69815f8a319
Devin <devinrobichaux@gmail.com>
2018-01-24 00:17:30 -0600

    [dummy] data

--- scheduleExample.js
+++ scheduleExample.js
@@ -3,18 +3,22 @@ module.exports.schedule1 = {
   day_1: [
     {
       name: 'Channel Gardens',
+      googleId: 'ChIJ5yWju_5YwokR6dgLgzNaIBw',
       location: { latitude: 40.7585013, longitude: -73.97808700000002 },
     },
     {
       name: 'The Metropolitan Museum of Art',
+      googleId: 'ChIJb8Jg9pZYwokR-qHGtvSkLzs',
       location: { latitude: 40.7794366, longitude: -73.963244 },
     },
     {
       name: 'Central Park',
+      googleId: 'ChIJ4zGFAZpYwokRGUGph3Mf37k',
       location: { latitude: 40.7828647, longitude: -73.9653551 },
     },
     {
       name: 'The American Museum of Natural History',
+      googleId: 'ChIJCXoPsPRYwokRsV1MYnKBfaI',
       location: { latitude: 40.7794366, longitude: -73.963244 },
     },
     {
@@ -29,19 +33,18 @@ module.exports.schedule1 = {
   day_2: [
     {
       name: 'Empire State Building',
+      googleId: 'ChIJaXQRs6lZwokRY6EFpJnhNNE',
       location: { latitude: 40.7484405, longitude: -73.98566439999999 },
     },
     {
       name: 'St. Patrick\'s Cathedral',
+      googleId: 'ChIJUW4vEPxYwokRW6o24DU0YIg',
       location: { latitude: 40.7584653, longitude: -73.97599269999999 },
     },
     {
       name: 'Radio City Music Hall',
+      googleId: 'ChIJPS8b1vhYwokRldqq2YHmxJI',
       location: { latitude: 40.75997599999999, longitude: -73.97997719999999 },
-    },
-    {
-      name: 'The High Line',
-      location: { latitude: 40.7479925, longitude: -74.0047649 },
     },
     {
       name: 'Shake Shack',
@@ -53,7 +56,6 @@ module.exports.schedule1 = {
     },
   ],
 }; // For individual schedule view
-
 module.exports.schedule2 = {
   name: 'New Donk City',
   day_1: [
@@ -109,6 +111,4 @@ module.exports.schedule2 = {
     },
   ],
 }; // For individual schedule view
-
 const getSchedules = [{ name: 'City name', id: 'schedule id' }]; // for dashboard page
-
